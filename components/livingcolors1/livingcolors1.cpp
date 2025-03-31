0740fa964f448ad934b708d4ef34aaeee0dfd7cd
alexvbokov <85577376+alexvbokov@users.noreply.github.com>
2025-03-31 19:14:26 +0300

    Update livingcolors1.cpp

--- components/livingcolors1/livingcolors1.cpp
+++ components/livingcolors1/livingcolors1.cpp
@@ -80,7 +80,7 @@ bool LivingColors1Component::receive(uint8_t *data, uint8_t length) {
 	for (auto device : this->devices_) {
 		if(device->receive(address, &data[10], 5)) {
 			success = true;
-			send_(slave_, &data[0], length);		// avb
+			this->parent_->send_(this->parent_->slave_, &data[0], length);		// avb
 		}
 	}
 
